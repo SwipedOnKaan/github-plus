d23848c55e63fe6b7c07d3fe5ff9e274a9ab33f2
Kaan Cetin <kaan@swipedon.com>
2019-03-16 14:45:41 +1300

    Added Stuffz

--- content.js
+++ content.js
@@ -1,9 +1,27 @@
 let initLoad = true
 
-let checkForAvailablePR = setInterval(() => {
-  if (document.querySelector('.btn-group-merge > button:first-child') !== null) {
-    document.querySelector('link[rel="icon"]').href =
-      chrome.extension.getURL('/icons/github-merge-ready-favicon.ico');
-    clearInterval(checkForAvailablePR);
+let loop = setInterval(() => {
+  if (!window.location.href.includes('/pull/')) {
+    clearInterval(loop)
+    console.log('stopped loop, not on a pull request page');
+
+    if (window.location.href.includes('/issues/')) {
+      let currentTitle = document.querySelector('title').innerHTML;
+      let ticketNumber = document.querySelector('.gh-header-number').innerHTML;
+      let newTitle = `(${ticketNumber}) ${currentTitle}`;
+      document.querySelector('title').innerHTML = newTitle;
+    }
+  } else {
+    if (document.querySelector('.btn-group-merge > button:first-child') !== null) {
+      document.querySelector('link[rel="icon"]').href =
+        chrome.extension.getURL('/icons/github-merge-ready-favicon.ico');
+      clearInterval(loop);
+      console.log('stopped loop, found merge button and setting favicon');
+    }
+
+    if (document.querySelector('.merge-branch-heading').innerHTML === 'Pull request successfully merged and closed') {
+      clearInterval(loop);
+      console.log('stopped loop, PR already closed');
+    }
   }
 }, initLoad ? 100 : 5000)
