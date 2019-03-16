d85eaeffdb7b6bb3132fb252cd73a02989b84660
Kaan Cetin <kaan@swipedon.com>
2019-03-17 11:00:07 +1300

    loops

--- content.js
+++ content.js
@@ -44,3 +44,13 @@ let loop = setInterval(() => {
     }
   }
 }, initLoad ? 100 : 2000)
+
+
+
+let loop2 = setInterval(() => {
+  let isDisabled = document.querySelector('.btn-group-merge > button:first-child').disabled
+  console.log('isDisabled?: ', isDisabled);
+  if (!isDisabled) {
+    clearInterval(loop2);
+  }
+}, 10)
