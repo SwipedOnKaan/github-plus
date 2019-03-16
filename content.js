b4a62834fdf296f6a0dbbeef56f6324c31cd30bb
Kaan Cetin <kaan@swipedon.com>
2019-03-16 14:25:01 +1300

    Added favicon change when merge is available

--- content.js
+++ content.js
@@ -1,5 +1,11 @@
 //This is a test commit
 
-let loop = setTimeout(() => {
-  console.log("Looking for 'Merge pull request' button text...");
-}, 5000)
+let loop = setInterval(function() {
+  if (document.querySelector('.btn-group-merge > button:first-child') !== null) {
+    console.log("Button found, stopping loop and setting favicon");
+    document.querySelector('link[rel="icon"]').href = chrome.extension.getURL('/icons/github-merge-ready-favicon.ico');
+    clearInterval(loop);
+  } else {
+    console.log("button not found");
+  }
+}, 100)
