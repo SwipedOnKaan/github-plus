3742afb8d39d652ec93fcac82122c53d7429d16f
Kaan Cetin <kaan@swipedon.com>
2019-03-16 14:28:25 +1300

    Added stuff

--- content.js
+++ content.js
@@ -1,11 +1,9 @@
-//This is a test commit
+let initLoad = true
 
-let loop = setInterval(function() {
+let checkForAvailablePR = setInterval(() => {
   if (document.querySelector('.btn-group-merge > button:first-child') !== null) {
-    console.log("Button found, stopping loop and setting favicon");
-    document.querySelector('link[rel="icon"]').href = chrome.extension.getURL('/icons/github-merge-ready-favicon.ico');
-    clearInterval(loop);
-  } else {
-    console.log("button not found");
+    document.querySelector('link[rel="icon"]').href =
+      chrome.extension.getURL('/icons/github-merge-ready-favicon.ico');
+    clearInterval(checkForAvailablePR);
   }
-}, 100)
+}, initLoad ? 100 : 5000)
